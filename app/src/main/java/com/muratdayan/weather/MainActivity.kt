4267182b6828727319207b9458c6fd57df405895
muratdayan <muratdayan359@gmail.com>
2024-05-17 14:45:28 +0300

    homefragment added navigation created

--- app/src/main/java/com/muratdayan/weather/MainActivity.kt
+++ app/src/main/java/com/muratdayan/weather/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.muratdayan.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+
+    }
+
+    //geri düğmesine basılırken stack'teki fragmentları kontrol eder
+    override fun onSupportNavigateUp(): Boolean {
+        return  navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/muratdayan/weather/presentation/screens/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.muratdayan.weather.presentation.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muratdayan.weather.R
+import com.muratdayan.weather.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    // fragmentlarda binding kullanımı
+    private var _binding:FragmentHomeBinding?=null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // binding bağlanması
+        _binding= FragmentHomeBinding.inflate(inflater,container,false)
+
+
+        binding.textView.setText("home fragment")
+
+
+
+
+        return binding.root
+    }
+
+    // fragmentimiz activity üzerinde kaybolduğunda bindingi null yaparız
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+
+
+}
