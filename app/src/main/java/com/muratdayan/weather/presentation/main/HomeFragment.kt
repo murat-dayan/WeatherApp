3f74ff7083783c56dd3c51c41ea69881698eeabe
muratdayan <muratdayan359@gmail.com>
2024-06-13 19:41:58 +0300

    feat: weather icons added and checked Icoon for weather model description

--- app/src/main/java/com/muratdayan/weather/presentation/main/HomeFragment.kt
+++ app/src/main/java/com/muratdayan/weather/presentation/main/HomeFragment.kt
@@ -20,6 +20,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.LocationServices
 import com.muratdayan.weather.R
+import com.muratdayan.weather.core.utils.checkWeatherforImage
 import com.muratdayan.weather.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -107,6 +108,10 @@ class HomeFragment : Fragment() {
                             currentModel.weather.first().description
                         val date = getMonthAndDayFromTimestamp(currentModel.dt)
                         binding.txtViewTodayDate.text = date
+
+                        currentModel.weather.forEach {
+                            binding.imageViewWeather.setImageResource(it.checkWeatherforImage())
+                        }
                     }
 
                     currentWeatherState.isLoading -> {
@@ -124,6 +129,7 @@ class HomeFragment : Fragment() {
                 when {
                     forecastState.forecastModel != null -> {
                         val forecastModel = forecastState.forecastModel
+
                         val firstHour = getTimeFromDateTime(forecastModel.forecastList[0].dtTxt)
                         val secondHour = getTimeFromDateTime(forecastModel.forecastList[1].dtTxt)
                         val thirdHour = getTimeFromDateTime(forecastModel.forecastList[2].dtTxt)
@@ -146,6 +152,15 @@ class HomeFragment : Fragment() {
                         binding.infoCardSecondHourRow.infoCardTxtTopInfo.text = secondHourTemp
                         binding.infoCardThirdHourRow.infoCardTxtTopInfo.text = thirdHourTemp
                         binding.infoCardFourthHourRow.infoCardTxtTopInfo.text = fourthHourTemp
+
+                        forecastModel.forecastList.forEach {
+                            it.weatherList.forEach {
+                                binding.infoCardFirstHourRow.imageView2.setImageResource(it.checkWeatherforImage())
+                                binding.infoCardSecondHourRow.imageView2.setImageResource(it.checkWeatherforImage())
+                                binding.infoCardThirdHourRow.imageView2.setImageResource(it.checkWeatherforImage())
+                                binding.infoCardFourthHourRow.imageView2.setImageResource(it.checkWeatherforImage())
+                            }
+                        }
 
 
 
