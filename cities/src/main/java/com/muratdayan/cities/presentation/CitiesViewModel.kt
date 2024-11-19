6884593e5da61c49cd284854b8e084294be7df30
muratdayan <muratdayan359@gmail.com>
2024-11-19 19:20:00 +0300

    feat: SearchCityUseCase class created and invoke search city and search city fun added in CitiesViewModel

--- cities/src/main/java/com/muratdayan/cities/presentation/CitiesViewModel.kt
+++ cities/src/main/java/com/muratdayan/cities/presentation/CitiesViewModel.kt
@@ -1,15 +1,29 @@
 package com.muratdayan.cities.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.muratdayan.cities.core.common.Resource
+import com.muratdayan.cities.domain.model.CityResponseModel
+import com.muratdayan.cities.domain.usecase.SearchCityUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 class CitiesViewModel @Inject constructor(
-
+    private val searchCityUseCase: SearchCityUseCase
 ) : ViewModel(){
 
+    private val _searchedCitiesList = MutableStateFlow<Resource<CityResponseModel>>(Resource.Idle())
+    val searchedCitiesList: StateFlow<Resource<CityResponseModel>> = _searchedCitiesList
 
 
-
+    fun searchCity(searchText:String){
+        searchCityUseCase.invoke(searchText).onEach { result->
+            _searchedCitiesList.value = result
+        }.launchIn(viewModelScope)
+    }
 }
