09a15f889d8b7e02b51290faaa29675e5d9cde54
muratdayan <muratdayan359@gmail.com>
2024-11-19 18:42:06 +0300

    feat: Provide retrofit instance and citiesservice and CitiesRepository

---
+++ cities/src/main/java/com/muratdayan/cities/core/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.muratdayan.cities.core.utils
+
+object Constants {
+    const val CITIES_BASE_URI = "https://api.thecompaniesapi.com/v2/"
+}
--- cities/src/main/java/com/muratdayan/cities/di/NetworkModule.kt
+++ cities/src/main/java/com/muratdayan/cities/di/NetworkModule.kt
@@ -1,4 +1,39 @@
 package com.muratdayan.cities.di
 
+import com.muratdayan.cities.core.utils.Constants
+import com.muratdayan.cities.data.remote.repository.CitiesRepositoryImpl
+import com.muratdayan.cities.data.remote.service.CitiesService
+import com.muratdayan.cities.domain.repository.CitiesRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(Constants.CITIES_BASE_URI)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideCitiesService(retrofit: Retrofit): CitiesService {
+        return retrofit.create(CitiesService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCitiesRepository(citiesService: CitiesService): CitiesRepository {
+        return CitiesRepositoryImpl(citiesService)
+    }
+
 }
