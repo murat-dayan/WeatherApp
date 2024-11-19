e60ceddd781b005087adeb015d255c7f2aee7d21
muratdayan <muratdayan359@gmail.com>
2024-11-19 19:13:40 +0300

    feat: search city functions declared in cityservice , CitiesRepository and CitiesRepositoryImpl

--- cities/src/main/java/com/muratdayan/cities/data/remote/repository/CitiesRepositoryImpl.kt
+++ cities/src/main/java/com/muratdayan/cities/data/remote/repository/CitiesRepositoryImpl.kt
@@ -1,10 +1,34 @@
 package com.muratdayan.cities.data.remote.repository
 
+import com.muratdayan.cities.core.common.Resource
+import com.muratdayan.cities.data.remote.mapper.toCityResponseModel
 import com.muratdayan.cities.data.remote.service.CitiesService
+import com.muratdayan.cities.domain.model.CityResponseModel
 import com.muratdayan.cities.domain.repository.CitiesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class CitiesRepositoryImpl @Inject constructor(
     private val citiesService: CitiesService
 ) : CitiesRepository{
+    override fun searchCities(searchText: String): Flow<Resource<CityResponseModel>> = flow {
+        emit(Resource.Loading())
+
+        try {
+            val cityResponseDto = citiesService.searchCity(searchText)
+            val cityResponseModel = cityResponseDto.toCityResponseModel()
+
+            emit(Resource.Success(cityResponseModel))
+
+        }catch (e:Exception){
+            emit(Resource.Error(e.message.toString()))
+        }
+    }.flowOn(Dispatchers.IO)
+        .catch {error->
+            emit(Resource.Error(error.message.toString()))
+        }
 }
--- cities/src/main/java/com/muratdayan/cities/domain/repository/CitiesRepository.kt
+++ cities/src/main/java/com/muratdayan/cities/domain/repository/CitiesRepository.kt
@@ -1,4 +1,9 @@
 package com.muratdayan.cities.domain.repository
 
+import com.muratdayan.cities.core.common.Resource
+import com.muratdayan.cities.domain.model.CityResponseModel
+import kotlinx.coroutines.flow.Flow
+
 interface CitiesRepository {
+    fun searchCities(searchText:String): Flow<Resource<CityResponseModel>>
 }
