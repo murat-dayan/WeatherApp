156d0e4c04f32121abd2be834ce4bf71577521fc
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-12-09 19:38:05 +0300

    style: Added details to the home fragment

--- app/src/main/java/com/muratdayan/weather/presentation/main/HomeFragment.kt
+++ app/src/main/java/com/muratdayan/weather/presentation/main/HomeFragment.kt
@@ -16,17 +16,23 @@ import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.location.LocationServices
 import com.muratdayan.common.utils.Resource
 import com.muratdayan.weather.databinding.FragmentHomeBinding
+import com.muratdayan.weather.presentation.weather_detail.WeatherDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.time.temporal.ChronoField
 import java.util.Locale
+import kotlin.collections.get
+import kotlin.toString
 
 // handle the UI and business logic of the HomeFragment
 @AndroidEntryPoint
@@ -38,6 +44,8 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val homeViewModel: HomeViewModel by viewModels()
+    private val weatherDetailViewModel: WeatherDetailViewModel by viewModels()
+
 
     private val permissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
@@ -55,6 +63,30 @@ class HomeFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.detailCardUvIndex.textViewSunrise.text = "UV INDEX"
+        binding.detailCardUvIndex.textViewSunsetTime.text = "Moderate"
+
+        binding.rvForecasts.setHasFixedSize(true)
+        binding.rvForecasts.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvForecasts.isNestedScrollingEnabled = false
+
+        weatherDetailViewModel.getDailyForecastWeather(52.52, 13.41)
+        collectProductStateDetail()
+
+        arguments?.let {
+            /*forecastModel = WeatherDetailFragmentArgs.fromBundle(it).forecastModel
+
+            forecastModel?.let {
+                binding.txtViewLocalArea.text = it.city.name
+                binding.detailCardSunrise.textViewSunriseTime.text =
+                    unixTimestampToTimeString(it.city.sunrise)
+                binding.detailCardSunrise.textViewSunsetTime.text =
+                    "Sunset: ${unixTimestampToTimeString(it.city.sunset)}"
+            }*/
+        }
+
         return binding.root
     }
 
@@ -72,6 +104,45 @@ class HomeFragment : Fragment() {
             permissionLauncher.launch(
                 arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
             )
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun unixTimestampToTimeString(timestamp: Long): String {
+        val instant = Instant.ofEpochSecond(timestamp)
+        val formatter = DateTimeFormatter.ofPattern("h:mm a", Locale.ENGLISH)
+            .withZone(ZoneId.systemDefault())
+        return formatter.format(instant)
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun collectProductStateDetail() {
+        lifecycleScope.launch {
+            weatherDetailViewModel.dailyModelState.collectLatest { dailyModelState ->
+                when {
+                    dailyModelState.dailymodel != null -> {
+                        val dailyModel = dailyModelState.dailymodel
+                        println(dailyModel.time[0])
+                        /*forecastModel?.let {
+                            binding.rvForecasts.adapter = DailyForecastAdapter(
+                                dailyModel,
+                                forecastModel!!
+                            )
+                        }*/
+
+                        binding.detailCardUvIndex.textViewSunriseTime.text = dailyModel.uv_index_max[0].toString()
+
+                    }
+
+                    dailyModelState.isLoading -> {
+                        Toast.makeText(requireContext(), "Loading...", Toast.LENGTH_SHORT).show()
+                    }
+
+                    else -> {
+                        Log.d("failure", "failure ${dailyModelState.error}")
+                    }
+                }
+            }
         }
     }
 
